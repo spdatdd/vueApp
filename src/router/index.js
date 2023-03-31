3c962ce69422d1928fdcc9c64543f66cea2cc095
spdatdd <ngod2810@gmail.com>
2023-03-31 12:04:50 +0700

    cai dat trang loi 404

---
+++ src/router/index.js
@@ -0,0 +1,23 @@
+import { createWebHashHistory, createRouter, createWebHistory } from "vue-router"
+import ContactBook from "@/views/ContactBook.vue"
+import NotFound from '../views/NotFound.vue'
+
+const routes = [
+    {
+        path: "/",
+        name: "contactbook",
+        component: ContactBook,
+    },
+    {
+        path: "/:pathMatch(.*)*",
+        name: "notfound",
+        component: NotFound,
+    }
+]
+
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes,
+})
+
+export default router
